d14cab1d13c77fe5359b84d21e26cc5658ba8e4b
luigi.oliveto@gmail.com <luigi.oliveto@gmail.com@f3ab62c7-01b6-2d47-c05e-156c54c3a90e>
2011-12-07 17:54:39 +0000

    new class for prova
    git-svn-id: http://svn.codespot.com/a/eclipselabs.org/oliveto-giani-client/myFinance@11 f3ab62c7-01b6-2d47-c05e-156c54c3a90e

---
+++ src/it/dev/JavaClass.java
@@ -0,0 +1,13 @@
+package it.dev;
+
+public class JavaClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ciao = "ciao";
+	}
+
+}
