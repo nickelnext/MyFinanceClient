aa19f384667b0f1c7e104d0c5be3dd5813e8fbf5
franco.maroni@gmail.com <franco.maroni@gmail.com@f3ab62c7-01b6-2d47-c05e-156c54c3a90e>
2012-01-12 18:17:39 +0000

    git-svn-id: http://svn.codespot.com/a/eclipselabs.org/oliveto-giani-client/myFinance@138 f3ab62c7-01b6-2d47-c05e-156c54c3a90e

--- src/it/util/ResponseHandler.java
+++ src/it/util/ResponseHandler.java
@@ -16,15 +16,15 @@ public class ResponseHandler {
 
 	
 	public static QuotationContainer decodeQuotations(String json) throws IllegalStateException{
-	//TODO modificare a seconda di come decideremo di comprimere/convertire le  richieste
-	QuotationContainer res;
-	Gson converter = new Gson();	
-	Type typeOfT = new TypeToken<QuotationContainer>(){}.getType();
-	res = converter.fromJson(json, typeOfT);	
-	return res;
+		//TODO modificare a seconda di come decideremo di comprimere/convertire le  richieste
+		QuotationContainer res;
+		Gson converter = new Gson();	
+		Type typeOfT = new TypeToken<QuotationContainer>(){}.getType();
+		res = converter.fromJson(json, typeOfT);	
+		return res;
 	}
 
-	
+
 	public static ArrayList<TypeSiteObject> decodeDBSiteType(String json) throws IllegalStateException{
 		//TODO modificare a seconda di come decideremo di comprimere/convertire le  richieste
 		ArrayList<TypeSiteObject> res;
