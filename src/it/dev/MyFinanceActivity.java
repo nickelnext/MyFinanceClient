89358fdb19a1b7eb8b6d9ef60a02c4ba32a9ddfd
luigi.oliveto@gmail.com <luigi.oliveto@gmail.com@f3ab62c7-01b6-2d47-c05e-156c54c3a90e>
2012-01-12 17:00:42 +0000

    update
    git-svn-id: http://svn.codespot.com/a/eclipselabs.org/oliveto-giani-client/myFinance@134 f3ab62c7-01b6-2d47-c05e-156c54c3a90e

--- src/it/dev/MyFinanceActivity.java
+++ src/it/dev/MyFinanceActivity.java
@@ -16,8 +16,6 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -42,37 +40,11 @@ public class MyFinanceActivity extends Activity
 	private ArrayList<String> portfolioNameArrayList = new ArrayList<String>();
 	private ArrayList<String> portfolioDescriptionArrayList = new ArrayList<String>();
 	private SupportDatabaseHelper supportDatabase = new SupportDatabaseHelper(this);
-	private Handler mHandler;
-
-	protected static final int FINISH_LOAD = 0 ;
-	protected static final int START_LOAD = 1 ;
-	protected static final int ABORT_LOAD = 2 ;
 
 	public void onCreate(Bundle savedInstanceState) 
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-
-		mHandler = new Handler() 
-		{
-			public void handleMessage(Message msg) {
-				switch (msg.what) {
-				case FINISH_LOAD:
-					Splashscreen.sendMessage(Splashscreen.CLOSE_SPLASH);
-					break;
-				case START_LOAD:
-					initializing();
-					break;
-				case ABORT_LOAD:
-					finish();
-				}
-			}
-		};
-		startSplash();
-
-
-
 
 		portfolioListView = (ListView) findViewById(R.id.portfolioListView);
 		registerForContextMenu(portfolioListView);
@@ -80,31 +52,6 @@ public class MyFinanceActivity extends Activity
 		db = new MyFinanceDatabase(this);
 
 		System.out.println(ConnectivityManager.EXTRA_EXTRA_INFO);
-	}
-
-	private void startSplash() 
-	{
-		Intent intent = new Intent(this, Splashscreen.class);
-		Splashscreen.setMainHandler(mHandler);
-		startActivity(intent);
-	}
-
-	private void initializing() 
-	{
-		new Thread () 
-		{
-			public void run() 
-			{
-				for (int i = 0; i < 10000; i++) 
-				{
-					Message msg = new Message();
-					msg.what = Splashscreen.SET_PROGRESS;
-					msg.arg1 = i;
-					Splashscreen.sendMessage(msg);
-				}
-				mHandler.sendEmptyMessage(FINISH_LOAD);
-			}
-		}.start();
 	}
 
 	public void onResume()
